0eef0f0519ed0616f5f5e3adce16a7fda3cf98e2
Alejandro Díaz <Alejandro.Daz@mailinator.com>
2019-03-15 14:35:26 -0700

    shuffle files/directories to match other projects

--- ui/autoVersion.sh
+++ ui/autoVersion.sh
@@ -3,19 +3,19 @@
 #
 # this little utility creates a teensy module to get the
 # version of the currently building code from git. the module
-# will be src/autoVersion.js, which should be in .gitignore
+# will be src/js/autoVersion.js, which should be in .gitignore
 #
 BRANCH=$(git branch | grep "*" | cut -d " " -f 2)
 VER=$(git describe --tags --long)
 current=""
-if [ -e src/autoVersion.txt ]; then
-    current=$(cat src/autoVersion.txt)
+if [ -e src/js/autoVersion.txt ]; then
+    current=$(cat src/js/autoVersion.txt)
 fi
 if [ "${current}" != "${BRANCH}-${VER}" ]; then
     echo "autoversion.sh: new version is ${BRANCH}-${VER}"
     echo "autoversion.sh: run make again..."
-    echo "${BRANCH}-${VER}" > src/autoVersion.txt
-    echo "var autoVersion = module.exports = { version() { return \"$BRANCH $VER\"; } }" > src/autoVersion.js
+    echo "${BRANCH}-${VER}" > src/js/autoVersion.txt
+    echo "var autoVersion = module.exports = { version() { return \"$BRANCH $VER\"; } }" > src/js/autoVersion.js
 else
     echo "autoversion.sh: version is unchanged"
 fi
